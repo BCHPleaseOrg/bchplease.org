b975f55ed501983ef0810e6ae49752280c3e4f09
Unknown User <nyusternie@sdot.io>
2020-03-19 15:44:42 -0400

    Updated sponsors.

--- src/components/welcome/Sponsors.vue
+++ src/components/welcome/Sponsors.vue
@@ -4,34 +4,45 @@
             <div class="row margin-bottom">
                 <div class="col-12">
                     <div class="heading heading--primary heading--center">
-                        <span class="heading__pre-title">Sponsors</span>
+                        <span class="heading__pre-title">Our Featured Sponsors</span>
 
                         <h2 class="heading__title no-margin-bottom">
-                            <span>Support Those</span> <span>Who Help Us</span>
+                            <span>Please Support Those</span>
+                            <br /><span>Who Support Us</span>
                         </h2>
                     </div>
                 </div>
             </div>
+
             <div class="row">
                 <div class="col-12">
-                    <!-- donors slider start-->
                     <div class="slider-holder">
                         <div class="donors-slider donors-slider--style-1">
                             <div class="donors-slider__item">
-                                <div class="donors-slider__img"><img src="@/assets/images/donor_1.png" alt="donor"/></div>
+                                <div class="donors-slider__img">
+                                    <img src="@/assets/images/donor_1.png" alt="donor"/>
+                                </div>
                             </div>
+
                             <div class="donors-slider__item">
-                                <div class="donors-slider__img"><img src="@/assets/images/donor_2.png" alt="donor"/></div>
+                                <div class="donors-slider__img">
+                                    <img src="@/assets/images/donor_2.png" alt="donor"/>
+                                </div>
                             </div>
+
                             <div class="donors-slider__item">
-                                <div class="donors-slider__img"><img src="@/assets/images/donor_3.png" alt="donor"/></div>
+                                <div class="donors-slider__img">
+                                    <img src="@/assets/images/donor_3.png" alt="donor"/>
+                                </div>
                             </div>
+
                             <div class="donors-slider__item">
-                                <div class="donors-slider__img"><img src="@/assets/images/donor_4.png" alt="donor"/></div>
+                                <div class="donors-slider__img">
+                                    <img src="@/assets/images/donor_4.png" alt="donor"/>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <!-- donors slider end-->
                 </div>
             </div>
         </div>
